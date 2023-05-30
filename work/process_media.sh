180f3d957a36d0c734e51175631ed6f0b777923e
Wenceslaus Dsilva <wenceslausdsilva@gmail.com>
2023-05-30 15:25:53 +0530

    Create output directory and files with proper naming conventions.

--- work/process_media.sh
+++ work/process_media.sh
@@ -12,9 +12,6 @@ main_dir=$1
 # Output directory
 output_dir="$main_dir/output"
 
-# Create the output directory if it doesn't exist
-mkdir -p "$output_dir"
-
 # Check if directory exists
 if [ ! -d "$main_dir" ]; then
     echo "Directory $main_dir does not exist."
@@ -33,8 +30,12 @@ for chapter_dir in "$main_dir"/*; do
 
                 # Check if both files exist
                 if [ -f "$video_file" ] && [ -f "$audio_file" ]; then
-                    # Create the output file name
-                    output_file="${output_dir}/$(basename "$content_dir") - Final.mp4"
+                    # Create the output directory for this content, maintaining the structure
+                    output_content_dir="${output_dir}/${chapter_dir##*/}/${content_dir##*/}"
+                    mkdir -p "$output_content_dir"
+
+                    # Create the output file name, replacing spaces with underscores
+                    output_file="${output_content_dir}/$(basename "$content_dir" | tr ' ' '_')_Final.mp4"
 
                     # Run the ffmpeg command
                     ffmpeg -hwaccel cuda -i "$video_file" -i "$audio_file" -map 0 -map 1 -c copy -metadata:s:a:0 title='Track 1' -metadata:s:a:1 title='Track 2' -metadata:s:a:0 language=hin -metadata:s:a:1 language=eng -strict -2 "$output_file"
