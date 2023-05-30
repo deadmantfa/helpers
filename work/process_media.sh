2157e0eddb25beca89580828e9dd6f94bddf41ab
Wenceslaus Dsilva <wenceslausdsilva@gmail.com>
2023-05-30 12:51:07 +0530

    Create output directory if it doesn't exist and fix output file path.

--- work/process_media.sh
+++ work/process_media.sh
@@ -9,6 +9,12 @@ fi
 # The main directory (CourseName)
 main_dir=$1
 
+# Output directory
+output_dir="$main_dir/output"
+
+# Create the output directory if it doesn't exist
+mkdir -p "$output_dir"
+
 # Check if directory exists
 if [ ! -d "$main_dir" ]; then
     echo "Directory $main_dir does not exist."
@@ -28,7 +34,7 @@ for chapter_dir in "$main_dir"/*; do
                 # Check if both files exist
                 if [ -f "$video_file" ] && [ -f "$audio_file" ]; then
                     # Create the output file name
-                    output_file="${content_dir}/$(basename "$content_dir") - Final.mp4"
+                    output_file="${output_dir}/$(basename "$content_dir") - Final.mp4"
 
                     # Run the ffmpeg command
                     ffmpeg -hwaccel cuda -i "$video_file" -i "$audio_file" -map 0 -map 1 -c copy -metadata:s:a:0 title='Track 1' -metadata:s:a:1 title='Track 2' -metadata:s:a:0 language=hin -metadata:s:a:1 language=eng -strict -2 "$output_file"
